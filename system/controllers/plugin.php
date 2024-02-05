7b05c9e979aaed043a6d66441dbafef89adb6ff1
freeispradius <158569053+freeispradius@users.noreply.github.com>
2024-02-05 05:32:05 +0300

    Update plugin.php

--- system/controllers/plugin.php
+++ system/controllers/plugin.php
@@ -4,6 +4,14 @@
  *  by https://t.me/freeispradius
  **/
 
+
+
+ header("Access-Control-Allow-Origin: *");
+ header("Access-Control-Allow-Methods: POST");
+ header("Access-Control-Allow-Headers: Content-Type");
+ 
+
+
 if(function_exists($routes[1])){
     call_user_func($routes[1]);
 }else{
