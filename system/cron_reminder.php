deeb32d26223f99a54e5710a4d2f2e0bc3e0006e
Gomez <magomere2019@gmail.com>
2024-05-11 01:34:42 +0300

    gfhfg

--- system/cron_reminder.php
+++ system/cron_reminder.php
@@ -22,36 +22,6 @@ if (php_sapi_name() !== 'cli') {
 // Rest of the script...
 
 //files were deleted here
-$d = ORM::for_table('tbl_user_recharges')->where('status', 'on')->find_many();
-run_hook('cronjob_reminder'); #HOOK
-
-echo "PHP Time\t" . date('Y-m-d H:i:s') . "\n";
-$res = ORM::raw_execute('SELECT NOW() AS WAKTU;');
-$statement = ORM::get_last_statement();
-$rows = array();
-while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-    echo "MYSQL Time\t" . $row['WAKTU'] . "\n";
-}
-
-$currentDate = date('Y-m-d');
-foreach ($d as $ds) {
-    $daysRemaining = (strtotime($ds['expiration']) - strtotime($currentDate)) / (60 * 60 * 24);
-    if ($daysRemaining >= 7 && $daysRemaining < 8) {
-        $u = ORM::for_table('tbl_user_recharges')->where('id', $ds['id'])->find_one();
-        $p = ORM::for_table('tbl_plans')->where('id', $u['plan_id'])->find_one();
-        $c = ORM::for_table('tbl_customers')->where('id', $ds['customer_id'])->find_one();
-        $price = Lang::moneyFormat($p['price']);
-        echo Message::sendPackageNotification($c, $p['name_plan'], $price, Lang::getNotifText('reminder_7_day'), $config['user_notification_reminder']) . "\n";
-    } else if ($daysRemaining >= 3 && $daysRemaining < 4) {
-        $u = ORM::for_table('tbl_user_recharges')->where('id', $ds['id'])->find_one();
-        $p = ORM::for_table('tbl_plans')->where('id', $u['plan_id'])->find_one();
-        $c = ORM::for_table('tbl_customers')->where('id', $ds['customer_id'])->find_one();
-        $price = Lang::moneyFormat($p['price']);
-        echo Message::sendPackageNotification($c, $p['name_plan'], $price, Lang::getNotifText('reminder_3_day'), $config['user_notification_reminder']) . "\n";
-    } else if ($daysRemaining >= 1 && $daysRemaining < 2) {
-        $u = ORM::for_table('tbl_user_recharges')->where('id', $ds['id'])->find_one();
-        $p = ORM::for_table('tbl_plans')->where('id', $u['plan_id'])->find_one();
-        $c = ORM::for_table('tbl_customers')->where('id', $ds['customer_id'])->find_one();
         $price = Lang::moneyFormat($p['price']);
         echo Message::sendPackageNotification($c, $p['name_plan'], $price, Lang::getNotifText('reminder_1_day'), $config['user_notification_reminder']) . "\n";
     }
